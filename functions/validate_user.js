2871b424c19789935c998a2a5abdd8b8474caa56
Sungjun Kim <hzcd108@gmail.com>
2022-06-16 19:54:57 +0900

    닉네임 유효성 검사 수정

--- functions/validate_user.js
+++ functions/validate_user.js
@@ -5,11 +5,16 @@ module.exports = function validateUser(nickname) {
     email: Joi.string().email().messages({
       'string.email': '이메일 양식을 지켜주세요',
     }),
-    nickname: Joi.string().alphanum().min(3).max(12).required().messages({
-      'string.alphanum': '알파벳 혹은 숫자만 사용해주세요',
-      'string.min': '닉네임은 최소 3글자 이상입니다.',
-      'string.max': '닉네임은 최대 12글자 이상입니다.',
-    }),
+    nickname: Joi.string()
+      .pattern(new RegExp(/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]+$/))
+      .min(3)
+      .max(12)
+      .required()
+      .messages({
+        'string.pattern.base': '한글, 숫자, 알파벳만 가능합니다.',
+        'string.min': '닉네임은 최소 3글자 이상입니다.',
+        'string.max': '닉네임은 최대 12글자 이상입니다.',
+      }),
     password: Joi.string()
       .custom((value, helpers) =>
         value.includes(nickname)
